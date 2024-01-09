b0834594d5d650761756e541caa445d9dfc64860
focusotter <mtliendo@focusotter.com>
2024-01-09 16:41:08 -0600

    update to working get call

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,15 @@
 import '@/styles/globals.css'
 import type { AppProps } from 'next/app'
+import { Amplify } from 'aws-amplify'
+import '@aws-amplify/ui-react/styles.css'
+import { config } from '@/awsconfig'
+
+Amplify.configure(config)
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+	return (
+		<>
+			<Component {...pageProps} />
+		</>
+	)
 }
